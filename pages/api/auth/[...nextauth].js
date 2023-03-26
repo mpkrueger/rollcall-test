3b59184b7a3db5b695f0917019179cb6bb1471d9
mpkrueger <mpkrueger@gmail.com>
2023-03-25 19:58:18 -0700

    trying to update auth file to work on vercel

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,23 +1,14 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export default NextAuth({
+export const authOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
     ],
-    pages: {
-        signIn: "/login",
-    },
-    callbacks: {
-        async signIn(user, account, profile) {
-            if (account.provider === "google" && profile.verified_email === true) {
-                return true;
-            }
+    
+}
 
-            return false;
-        },
-    },
-});
+export default NextAuth(authOptions)
